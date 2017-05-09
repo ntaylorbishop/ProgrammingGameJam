6e75cf10cc686bfd267cfc8c0f99468d8521777e
Taylor Bishop <ntaylorbishop@gmail.com>
2017-05-09 15:56:44 -0500

    nbishop: pushing solution

---
+++ Source/JrEngineerSim2017/MyClassouea.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "JrEngineerSim2017.h"
+#include "MyClassouea.h"
+
+MyClassouea::MyClassouea()
+{
+}
+
+MyClassouea::~MyClassouea()
+{
+}
---
+++ Source/JrEngineerSim2017/MyClassouea.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class JRENGINEERSIM2017_API MyClassouea
+{
+public:
+	MyClassouea();
+	~MyClassouea();
+};
